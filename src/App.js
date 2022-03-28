3bf59acf9705e4aa7e8503976f0ce775dc5461c5
guzzler13 <stephanedasil@gmail.com>
2022-03-28 10:16:22 +0200

    add useState

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import "./App.css"
 import { useState } from "react"
 
 function App() {
+  const [value, updateValue] = useState([
+    "https://media.giphy.com/media/l0MYJnJQ4EiYLxvQ4/giphy.gif",
+  ])
+
+  /**
+   * (Déstructuration en javascript) - Même chose que :
+   *
+   * const state = useState([])
+   * const value = state[0]
+   * const updateValue = state[1]
+   */
+
   return (
     <div className="App">
-      <section className="App-content">site vierge</section>
+      <section className="App-content">
+        <img src={value} alt="gif" />
+      </section>
     </div>
   )
 }
